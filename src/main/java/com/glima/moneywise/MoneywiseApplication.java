83efa36da432ace7be7da0b0ccb70540e558675a
Guilherme Lima <guilhermelima.iba@gmail.com>
2017-10-28 12:52:52 -0200

    First commit creating the project

---
+++ src/main/java/com/glima/moneywise/MoneywiseApplication.java
@@ -0,0 +1,12 @@
+package com.glima.moneywise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneywiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneywiseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/glima/moneywise/MoneywiseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.glima.moneywise;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneywiseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
