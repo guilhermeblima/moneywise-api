e2c65c127b77fcbfe0d6236eb4a5d1c47ce774b6
Guilherme Lima <guilhermelima.iba@gmail.com>
2017-11-20 11:40:57 +1300

    Adding new validation for invalid fields

--- src/main/java/com/glima/moneywise/exception/MoneywiseResourceException.java
+++ src/main/java/com/glima/moneywise/exception/MoneywiseResourceException.java
@@ -7,9 +7,16 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ControllerAdvice
 public class MoneywiseResourceException extends ResponseEntityExceptionHandler{
@@ -20,7 +27,21 @@ public class MoneywiseResourceException extends ResponseEntityExceptionHandler{
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         String friendlyMessage = messageSource.getMessage("invalid.parameter.request", null , LocaleContextHolder.getLocale());
-        return handleExceptionInternal(ex, new Error(friendlyMessage, ex.getCause().toString()), headers, HttpStatus.BAD_REQUEST, request);
+        return handleExceptionInternal(ex, Arrays.asList(new Error(friendlyMessage, ex.getCause().toString())), headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(ex,createErrorList(ex.getBindingResult()), headers, HttpStatus.BAD_GATEWAY, request);
+    }
+
+    private List<Error> createErrorList(BindingResult bindingResult) {
+        List<Error> errors = new ArrayList<>();
+        for(FieldError fieldError: bindingResult.getFieldErrors()){
+            String friendlyMessage = messageSource.getMessage(fieldError,LocaleContextHolder.getLocale());
+            errors.add(new Error(friendlyMessage, fieldError.toString()));
+        }
+        return errors;
     }
 
     public static class Error{
