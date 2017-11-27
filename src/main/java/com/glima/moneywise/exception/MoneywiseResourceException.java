cf9d1cd2de103db3cdb1f443e99e1ca334b99026
Guilherme Lima <guilhermelima.iba@gmail.com>
2017-11-28 11:42:34 +1300

    Exception handler for EmptyResultDataAccessException

--- src/main/java/com/glima/moneywise/exception/MoneywiseResourceException.java
+++ src/main/java/com/glima/moneywise/exception/MoneywiseResourceException.java
@@ -3,6 +3,7 @@ package com.glima.moneywise.exception;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -33,6 +35,12 @@ public class MoneywiseResourceException extends ResponseEntityExceptionHandler{
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         return handleExceptionInternal(ex,createErrorList(ex.getBindingResult()), headers, HttpStatus.BAD_GATEWAY, request);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,WebRequest request){
+        String friendlyMessage = messageSource.getMessage("resource.not.found", null , LocaleContextHolder.getLocale());
+        return handleExceptionInternal(ex,Arrays.asList(new Error(friendlyMessage, ex.toString())), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     private List<Error> createErrorList(BindingResult bindingResult) {
