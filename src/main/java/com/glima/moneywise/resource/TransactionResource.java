fc9922451318b5d33b6e0e40f7d70715d55d7a0c
Guilherme Lima <guilhermelima.iba@gmail.com>
2017-12-02 14:19:17 +1300

    save method added to transaction resource

--- src/main/java/com/glima/moneywise/resource/TransactionResource.java
+++ src/main/java/com/glima/moneywise/resource/TransactionResource.java
@@ -1,12 +1,16 @@
 package com.glima.moneywise.resource;
 
+import com.glima.moneywise.event.CreatedResourceEvent;
 import com.glima.moneywise.model.Transaction;
 import com.glima.moneywise.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -19,6 +23,9 @@ public class TransactionResource {
     @Autowired
     private TransactionRepository transactionRepository;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public List<Transaction> listAll(){
@@ -29,5 +36,12 @@ public class TransactionResource {
     public ResponseEntity<Transaction> findOne(@PathVariable Long id){
         Transaction transaction = transactionRepository.findOne(id);
         return transaction != null ? ResponseEntity.ok(transaction) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Transaction> save(@Valid @RequestBody Transaction transaction, HttpServletResponse response){
+        transaction = transactionRepository.save(transaction);
+        publisher.publishEvent(new CreatedResourceEvent(this,response,transaction.getId()));
+        return ResponseEntity.status(HttpStatus.CREATED).body(transaction);
     }
 }
