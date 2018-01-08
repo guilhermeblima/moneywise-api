3fa2e5dbd3ccc9f20c8a60b989bc734a9f129257
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-01-08 15:14:36 +1300

    refresh token added

--- src/main/java/com/glima/moneywise/config/AuthorizationServerConfig.java
+++ src/main/java/com/glima/moneywise/config/AuthorizationServerConfig.java
@@ -32,8 +32,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("angular")
                 .secret("@ngul@r0")
                 .scopes("read","write")
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(1800);
+                .authorizedGrantTypes("password", "refresh_token")
+                .accessTokenValiditySeconds(20)
+                .refreshTokenValiditySeconds(3600 * 24);
     }
 
     @Override
@@ -41,6 +42,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         endpoints
                 .tokenStore(tokenStore())
                 .accessTokenConverter(accessTokenConverter())
+                .reuseRefreshTokens(false)
                 .authenticationManager(authenticationManager);
     }
 
