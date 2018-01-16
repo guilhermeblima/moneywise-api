0b8ea68bee0457eac00eac0ba46d968b73ec4320
Guilherme Lima <guilhermelima.iba@gmail.com>
2018-01-17 12:53:48 +1300

    UserDetails created loading authorities

--- src/main/java/com/glima/moneywise/repository/UserRepository.java
+++ src/main/java/com/glima/moneywise/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.glima.moneywise.repository;
 import com.glima.moneywise.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * Created by Guilherme on 17/01/2018.
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
---
+++ src/main/java/com/glima/moneywise/security/AppUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.glima.moneywise.security;
+
+import com.glima.moneywise.model.User;
+import com.glima.moneywise.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+/**
+ * Created by Guilherme on 17/01/2018.
+ */
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("Username and/or Password incorrect."));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthorities(user));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        user.getRoles().forEach(r -> authorities.add(new SimpleGrantedAuthority(r.getName().toUpperCase())));
+        return authorities;
+    }
+}
